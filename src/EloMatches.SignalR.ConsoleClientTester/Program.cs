4cd2e7b9e504d3d56b3153e339926bd5fd5d5eb7
arnarinn1 <arnarh91@gmail.com>
2020-11-21 11:07:57 +0000

    SignalR scaffolding

---
+++ src/EloMatches.SignalR.ConsoleClientTester/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EloMatches.SignalR.ConsoleClientTester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/EloMatches.SignalR/Hubs/NotificationHub.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace EloMatches.SignalR.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        
+    }
+}
